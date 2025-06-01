87df5c4e3bdc54267bf3712c5e50a06b267f7e07
冯清水 <15408619+feng-jianhuai@user.noreply.gitee.com>
2025-06-01 10:12:31 +0800

    feat(权限管理): 添加权限列表展示及操作功能
    - 在权限管理页面添加表格展示权限名称、权限值、修改时间
    - 新增编辑和删除操作按钮
    - 实现获取权限列表数据的功能
    - 使用onMounted钩子在组件挂载时自动获取数据

--- src/views/acl/permission/index.vue
+++ src/views/acl/permission/index.vue
@@ -1,11 +1,36 @@
 <template>
     <div>
-        <el-table style="width:100%;margin-bottom:20px" row-key='id' border></el-table>
+        <el-table style="width:100%;margin-bottom:20px" row-key='id' border>
+          <el-table-column prop='name' label="权限名称" />
+          <el-table-column prop='code' label="权限值" />
+          <el-table-column prop='updateTime' label="修改时间" />
+          <el-table-column label="操作">
+            <template #default="{ row,$index }">
+              <el-button type="text" @click="">编辑</el-button>
+              <el-button type="text" @click="">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
     </div>
 </template>
 
 <script setup lang='ts'>
+import { reqAllPermission } from '@/api/acl/menu';
+import type { PermissionList, PermissionResponseData } from '@/api/acl/menu/type';
+import { onMounted, ref } from 'vue';
 
+let permissionArr= ref<PermissionList>();
+
+onMounted(() => {
+  getHasPermission();
+});
+
+const getHasPermission = async () => {
+ let result:PermissionResponseData =  await reqAllPermission();
+  if (result.code === 200) {
+      permissionArr.value = result.data;
+    }
+}
 </script>
 
 <style lang="scss" scoped></style>
